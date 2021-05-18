4c95492cfc991cfdb5dbf162b81ebac7434063b8
Pavel Nakonechnyi <pavel@zorg1331.one>
2021-05-18 22:40:10 +0200

    xilinx: add PYNQ-Z2 board support (zynq_pynq_z2)
    credits1:
     https://github.com/Xilinx/PYNQ/
     boards/Pynq-Z2/petalinux_bsp/meta-user/recipes-bsp/u-boot/files/
    
    credits2:
     https://zhouyuqian.com/2020/03/30/ZYNQ-%E7%A7%BB%E6%A4%8D-Linux/

---
+++ include/configs/zynq_pynqz2.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_ZYNQ_PYNQZ2_H
+#define __CONFIG_ZYNQ_PYNQZ2_H
+
+#define CONFIG_ZYNQ_I2C0
+
+/* Define PYNQZ2 PS Clock Frequency to 50MHz */
+#define CONFIG_ZYNQ_PS_CLK_FREQ 50000000UL
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_PYNQZ2_H */
